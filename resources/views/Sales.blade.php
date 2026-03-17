c2716da125e7429870451e416b448264aa4cf43b
jiroammarwafi <149659337+jiroammarwafi@users.noreply.github.com>
2026-03-18 02:51:00 +0700

    Jobsheet 2/Soal Praktikum/Keterangan: Route, Controller,dan View untuk Halaman Penjualan

---
+++ resources/views/Sales.blade.php
@@ -0,0 +1,5 @@
+<html>
+	<body>
+    	<h1>Ini adalah halaman untuk menampilkan transaksi penjualan</h1>
+	</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\SalesController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -19,3 +20,6 @@ Route::get ('/products/category/baby-kids', [ProductController::class, 'ctgBabyK
 
 // Route Halaman User
 Route::get ('/user/{id}/name/{name}', [UserController::class, 'show']);
+
+// Route Halaman Penjualan
+Route::get ('/sales', [SalesController::class, 'Sales']);
