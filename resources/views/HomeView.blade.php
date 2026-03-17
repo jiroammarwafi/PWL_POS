db0a8ab4ccf8127075d415d3558ad730484fda22
jiroammarwafi <149659337+jiroammarwafi@users.noreply.github.com>
2026-03-18 00:01:37 +0700

    Jobsheet 2/Soal Praktikum/Keterangan: Route, Controller,dan  View untuk Halaman Home

---
+++ resources/views/HomeView.blade.php
@@ -0,0 +1,7 @@
+<!-- View pada resources/views/hello.blade.php -->
+<html>
+	<body>
+    	<h1>Selamat datang di Home, {{ $name }}</h1>
+        <p>Ini adalah halaman Home dari POS</p>
+	</body>
+</html>
