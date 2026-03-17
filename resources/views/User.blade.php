0d146ee1d5530af285c70144e6ea2bfcdc2fe92e
jiroammarwafi <149659337+jiroammarwafi@users.noreply.github.com>
2026-03-18 02:46:43 +0700

    Jobsheet 2/Soal Praktikum/Keterangan: Route, Controller,dan View untuk Halaman User

---
+++ resources/views/User.blade.php
@@ -0,0 +1,7 @@
+<html>
+	<body>
+    	<h1>Data User</h1>
+        <p>User ID: {{ $id }}</p>
+        <p>Name: {{ $name }}</p>
+	</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,21 @@
 <?php
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+// Route Halaman Home
 Route::get ('/home', [HomeController::class, 'home']);
 
+// Route Halaman Products
 Route::get ('/products/category/food-beverage', [ProductController::class, 'ctgFoodBeverage']);
-
 Route::get ('/products/category/beauty-health', [ProductController::class, 'ctgBeautyHealth']);
-
 Route::get ('/products/category/home-care', [ProductController::class, 'ctgHomeCare']);
-
 Route::get ('/products/category/baby-kids', [ProductController::class, 'ctgBabyKids']);
+
+// Route Halaman User
+Route::get ('/user/{id}/name/{name}', [UserController::class, 'show']);
