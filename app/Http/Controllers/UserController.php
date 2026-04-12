99a18509b70dab5919df9ac80b499dd69a262d92
jiroammarwafi <149659337+jiroammarwafi@users.noreply.github.com>
2026-04-13 01:07:09 +0700

    JS4/Praktikum 2.3/Retrieving Aggreagates (4/4)

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,8 +10,13 @@ class UserController extends Controller
 {
     public function index()
 {
-    // $user = UserModel::findOrFail(1);// Mencari user dengan ID 1, jika tidak ditemukan akan menghasilkan error 404
-    $user = UserModel::where('username', 'manager9')->firstOrFail(); // Mencari user dengan username 'manager9', jika tidak ditemukan akan menghasilkan error 404
+    /* 
+        Praktikum 2.2: Not Found Exceptions
+        $user = UserModel::findOrFail(1);// Mencari user dengan ID 1, jika tidak ditemukan akan menghasilkan error 404
+        $user = UserModel::where('username', 'manager9')->firstOrFail(); // Mencari user dengan username 'manager9', jika tidak ditemukan akan menghasilkan error 404
+    */
+    $user = UserModel::where('level_id', 2)->count(); // Menghitung jumlah user dengan level_id 2
+    // dd($user); // Menampilkan hasil perhitungan jumlah user dengan level_id 2
     return view('user', ['data' => $user]); // Mengirim data user ke view 'user'   
 }
 }
