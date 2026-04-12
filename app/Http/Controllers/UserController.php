2ff02700a2830e61d5326b37d1ef57af22afab6e
jiroammarwafi <149659337+jiroammarwafi@users.noreply.github.com>
2026-04-13 02:12:02 +0700

    JS4/Praktikum 2.3/Retrieving or Creating Models (12/12)

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,13 +10,24 @@ class UserController extends Controller
 {
     public function index()
 {
-    /* 
-        Praktikum 2.2: Not Found Exceptions
-        $user = UserModel::findOrFail(1);// Mencari user dengan ID 1, jika tidak ditemukan akan menghasilkan error 404
-        $user = UserModel::where('username', 'manager9')->firstOrFail(); // Mencari user dengan username 'manager9', jika tidak ditemukan akan menghasilkan error 404
-    */
-    $user = UserModel::where('level_id', 2)->count(); // Menghitung jumlah user dengan level_id 2
-    // dd($user); // Menampilkan hasil perhitungan jumlah user dengan level_id 2
+    // $user = UserModel::firstOrCreate(
+    //     [
+    //         'username' => 'manager22',
+    //         'nama' => 'Manager Dua Dua',
+    //         'password' => Hash::make('12345'), 
+    //         'level_id' => 2
+    //     ], // Jika tidak ada, maka buat record baru dengan data tersebut.
+    // );
+
+    $user = UserModel::firstOrNew(
+        [
+            'username' => 'manager33',
+            'nama' => 'Manager Tiga Tiga',
+            'password' => Hash::make('12345'),
+            'level_id' => 2
+        ], // Mencari record dengan data tersebut.
+    );
+    $user ->save(); // Menyimpan record ke database jika belum ada, atau memperbarui jika sudah ada.
     return view('user', ['data' => $user]); // Mengirim data user ke view 'user'   
 }
 }
--- resources/views/User.blade.php
+++ resources/views/User.blade.php
@@ -7,10 +7,16 @@
     <h1>Data User</h1>
     <table border="1" cellpadding="2" cellspacing="0">
         <tr>
-            <th>Jumlah Pengguna</th>
+            <th>User ID</th>
+            <th>Username</th>
+            <th>Nama</th>
+            <th>Level ID</th>
         </tr>
         <tr>
-            <td>{{ $data }}</td>
+            <td>{{ $data->user_id }}</td>
+            <td>{{ $data->username }}</td>
+            <td>{{ $data->nama }}</td>
+            <td>{{ $data->level_id }}</td>
         </tr>
     </table>
 </body>
