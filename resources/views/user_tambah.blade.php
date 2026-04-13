25a59854e120df0718347d1faa07f248e84c2077
jiroammarwafi <149659337+jiroammarwafi@users.noreply.github.com>
2026-04-13 15:22:10 +0700

    JS4/Praktikum 2.6/CRUD: Create (10/21)

--- resources/views/user_tambah.blade.php
+++ resources/views/user_tambah.blade.php
@@ -1,6 +1,6 @@
 <body>
     <h1>Form Tambah Data User</h1>
-    <form method="post" action="/user/tambah_simpan">
+    <form method="post" action="tambah_simpan">
 
         {{ csrf_field() }}
 
--- routes/web.php
+++ routes/web.php
@@ -33,3 +33,5 @@ Route::get('/kategori', [KategoriController::class, 'index']);
 Route::get('/user', [UserController::class, 'index']);
 
 Route::get('/user/tambah', [UserController::class, 'tambah']);
+
+Route::post('/user/tambah_simpan', [UserController::class, 'tambah_simpan']);
